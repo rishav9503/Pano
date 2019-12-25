bd9c021e935e40f1782b173eed20a381c8c33ad3
Rishav <rishav@opsadmins-MacBook-Pro.local>
2019-12-25 18:28:19 +0530

    removed unnecessary codes

--- App.js
+++ App.js
@@ -29,6 +29,7 @@ import 'react-native-gesture-handler';
 
 import {createAppContainer} from 'react-navigation';
 import {createStackNavigator} from 'react-navigation-stack';
+console.disableYellowBox = true;
 const RootStack = createStackNavigator(
   {
     Home: {
@@ -64,43 +65,5 @@ const App: () => React$Node = () => {
   );
 };
 
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
 
 export default App;
